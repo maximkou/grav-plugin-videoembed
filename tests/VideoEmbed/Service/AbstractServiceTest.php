ba9e230ffd48a813ebbd6c31304d44dee95f1678
maximkou <maksim.khodyrev@nutnet.ru>
2014-11-28 17:19:21 +0400

    Added tests for processHtml

--- tests/VideoEmbed/Service/AbstractServiceTest.php
+++ tests/VideoEmbed/Service/AbstractServiceTest.php
@@ -23,7 +23,7 @@ abstract class AbstractServiceTest extends \PHPUnit_Framework_TestCase
     public function testGetEmbedNodes($vId, $urlQuery = null)
     {
         $service = $this->getMock(
-            '\\Grav\\Plugin\\VideoEmbed\\Service\\'.$this->getPluginName(),
+            $this->getPluginClass(),
             ['prepareStandardEmbed']
         );
 
@@ -39,11 +39,60 @@ abstract class AbstractServiceTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * @dataProvider dpProcessHtml
+     */
+    public function testProcessHtml($config, $inputHtml, $expectedHtml, $container = null)
+    {
+        $pluginClass = $this->getPluginClass();
+
+        $plugin = new $pluginClass($config);
+        $this->assertEquals(
+            preg_replace('/\n/', '', $expectedHtml),
+            preg_replace('/\n/', '', $plugin->processHtml($inputHtml, $container))
+        );
+    }
+
+    public function dpProcessHtml()
+    {
+        $htmlDir = __DIR__.'/../../_data/VideoEmbed/Service/'.ucfirst($this->getPluginName());
+
+        $doc = new \DOMDocument();
+        $data = [];
+        foreach (glob($htmlDir.'/*', GLOB_ONLYDIR) as $case) {
+            $elem = null;
+
+            if (file_exists($case.'/container.json')) {
+                $c = json_decode(file_get_contents($case.'/container.json'), true);
+                $elem = $doc->createElement($c['name']);
+                if (!empty($c['attr'])) {
+                    foreach ($c['attr'] as $attr => $attrVal) {
+                        $elem->setAttribute($attr, $attrVal);
+                    }
+                }
+            }
+
+            $data[] = [
+                json_decode(file_get_contents($case.'/options.json'), true),
+                file_get_contents($case.'/input.html'),
+                file_get_contents($case.'/output.html'),
+                $elem
+            ];
+        }
+
+        return $data;
+    }
+
     public function dpGetEmbedNode()
     {
         return [
             [ 'videoId', 'urlQuery'],
             ['videoId2']
         ];
+    }
+
+    protected function getPluginClass()
+    {
+        return '\\Grav\\Plugin\\VideoEmbed\\Service\\'.$this->getPluginName();
     }
 }
